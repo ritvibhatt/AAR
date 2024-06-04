1388a0911c3c40a1a42fa4462c8938852b1b61af
Andrew Kwon <andrew_kwon@ucsb.edu>
2024-06-03 17:59:34 -0700

    changes

--- HW3/VR Room/Assets/AndrewTut/Detect.cs
+++ HW3/VR Room/Assets/AndrewTut/Detect.cs
@@ -12,20 +12,25 @@ public class Detect : MonoBehaviour
     [SerializeField] private List<GameObject> right = new List<GameObject>();
     [SerializeField] private List<GameObject> wrong = new List<GameObject>();
     [SerializeField] private TextMeshPro text;
+    [SerializeField] private TextMeshPro timerText;
     [SerializeField] private TextMeshPro succText;
     public bool correctNotes = false;
     public string pressedKey;
     private int currentKey = 0;
     private List<GameObject> use;
+
+    public float timer;
+    private float insideTimer = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = insideTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer -= Time.deltaTime;
         if (correctNotes)
         {
             use = right;
@@ -36,17 +41,27 @@ public class Detect : MonoBehaviour
             use = wrong;
         }
         DisplayNote(currentKey);
+        DisplayTimer();
         Debug.Log(currentKey);
+        if (timer <= 0) 
+        {
+            currentKey++;
+            use[currentKey - 1].SetActive(false);
+            insideTimer++;
+            timer = insideTimer;
+        }
         if (pressedKey == song[currentKey])
         {
             Debug.Log(pressedKey);
             currentKey++;
             use[currentKey - 1].SetActive(false);
+            timer = insideTimer;
             
         }
         if (currentKey == (song.Count - 1))
         {
             text.gameObject.SetActive(false);
+            timerText.gameObject.SetActive(false);
             succText.gameObject.SetActive(true);
         }
     }
@@ -55,6 +70,11 @@ public class Detect : MonoBehaviour
     {
         text.text = $"Note To Play: {song[i]}";
         use[i].SetActive(true);
+    }
+
+    public void DisplayTimer()
+    {
+        timerText.text = $"Timer: {Mathf.FloorToInt(timer)}";
     }
 
     public void getKey(string key)
